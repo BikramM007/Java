7fc6627ce1c967e47d250620b8e4359c52c512e5
BikramM007 <mahatobikram720@gmail.com>
2023-05-10 01:14:03 +0530

    1st one

---
+++ dp/Prims_algorithm.java
@@ -0,0 +1,37 @@
+/*
+package dp;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+class Pair implements Comparable<Pair>{
+    int wt;
+    int v;
+    Pair(int v, int wt){
+        this.v = v;
+        this.wt = wt;
+    }
+    @Override
+    public int compareTo(Pair that) {
+        return this.wt-that.wt;
+    }
+}
+
+class solution{
+    static int spanningTree(int v, ArrayList<ArrayList<ArrayList<Integer>>>ad){
+        boolean[] vis = new boolean[v];
+        PriorityQueue<Pair> q = new PriorityQueue<>();
+
+        q.add(new Pair(0,0));
+        int ans = 0;
+        while(q.size()!= 0){
+            Pair cur
+        }
+
+    }
+}
+public class Prims_algorithm {
+
+
+}
+*/
